bd5eea012b8cc9ed85d22fb3f47e0954e0d92023
Jiratin <jiratin@amity.co>
2023-07-27 18:58:34 +0700

    [Adjust] reverse logic cal content height

--- UpstraUIKit/UpstraUIKit/Modules/Comunity/OneKTBActivityDetail/ViewController/OneKTBActivityDetailViewController.swift
+++ UpstraUIKit/UpstraUIKit/Modules/Comunity/OneKTBActivityDetail/ViewController/OneKTBActivityDetailViewController.swift
@@ -236,7 +236,15 @@ open class OneKTBActivityDetailViewController: AmityViewController {
     }
     
     private func updateContentHeight() {
-        delegate?.childViewController(self, didUpdateContentHeight: 150)
+        let screenHeight = UIScreen.main.bounds.height
+        let contentHeight = tableView.contentSize.height
+        let maxHeight = screenHeight // Custom offset if needed
+        
+        if contentHeight > maxHeight {
+            delegate?.childViewController(self, didUpdateContentHeight: maxHeight)
+        } else {
+            delegate?.childViewController(self, didUpdateContentHeight: contentHeight + 30)
+        }
     }
     
     private func openCommentView() {
