3fbfba6b219e3656a73cbdc524d355c94c3d96a2
thanaphat-amity <thanapat@convolab.ai>
2023-12-14 12:43:12 +0700

    Set avatar view is hidden when set up and show when load channel data complete, Decrease delay for waiting user presence

--- UpstraUIKit/UpstraUIKit/Modules/Chat/Message/ViewController/Container/AmityMessageListHeaderView.swift
+++ UpstraUIKit/UpstraUIKit/Modules/Chat/Message/ViewController/Container/AmityMessageListHeaderView.swift
@@ -57,6 +57,7 @@ private extension AmityMessageListHeaderView {
         displayNameLabel.text = nil
         
         avatarView.image = nil
+        avatarView.isHidden = true
         avatarView.placeholder = AmityIconSet.defaultAvatar
         
         /* [Custom for ONE Krungthai] Setup custom properties */
@@ -79,6 +80,8 @@ private extension AmityMessageListHeaderView {
 extension AmityMessageListHeaderView {
     
     func updateViews(channel: AmityChannelModel, isOnline: Bool) {
+        avatarView.isHidden = false
+        
         switch channel.channelType {
         case .standard:
             avatarView.setImage(withImageURL: channel.avatarURL, placeholder: AmityIconSet.defaultGroupChat)
