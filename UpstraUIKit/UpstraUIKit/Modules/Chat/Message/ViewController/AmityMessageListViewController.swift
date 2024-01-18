96229618abcbe4e491203bfb4303138c9fca28a7
thanaphat-amity <thanapat@convolab.ai>
2024-01-18 11:41:57 +0700

    Improve sync and un sync user presence

--- UpstraUIKit/UpstraUIKit/Modules/Chat/Message/ViewController/AmityMessageListViewController.swift
+++ UpstraUIKit/UpstraUIKit/Modules/Chat/Message/ViewController/AmityMessageListViewController.swift
@@ -235,10 +235,12 @@ public final class AmityMessageListViewController: AmityViewController {
                                                selector: #selector(refreshPresence(notification:)),
                                                name: Notification.Name("RefreshChannelPresence"),
                                                object: nil)
+        screenViewModel.action.syncChannelPresence()
     }
     
     private func stopObserver() {
         NotificationCenter.default.removeObserver(self, name: Notification.Name("RefreshChannelPresence"), object: nil)
+        screenViewModel.action.unsyncChannelPresence()
     }
     
     @objc func refreshPresence(notification: Notification) {
