0ed2959a68c816503d64d705c5a51ef0d5b61dbe
jakkapong <365-kkpp88@users.noreply.vayuscmdev.ktbcs.co.th>
2024-05-08 14:47:12 +0700

    fix screen empty when get fail userNotFound

--- UpstraUIKit/UpstraUIKit/Modules/Comunity/Feed Posts/Scenes/Feed/View Model/AmityFeedScreenViewModel.swift
+++ UpstraUIKit/UpstraUIKit/Modules/Comunity/Feed Posts/Scenes/Feed/View Model/AmityFeedScreenViewModel.swift
@@ -11,6 +11,10 @@ import AmitySDK
 
 final class AmityFeedScreenViewModel: AmityFeedScreenViewModelType {
     
+    // MARK: - KTB FOR FEED HOME
+    /// for check userNotFound and screen empty
+    var isKTBFeed = false
+    
     // MARK: - Delegate
     weak var delegate: AmityFeedScreenViewModelDelegate?
     
@@ -221,7 +225,9 @@ extension AmityFeedScreenViewModel {
                         }
                     case .failure(let error):
                         strongSelf.debouncer.run {
-                            strongSelf.prepareComponents(posts: [])
+                            if let isKTBFeed = self?.isKTBFeed, !isKTBFeed{
+                                strongSelf.prepareComponents(posts: [])
+                            }
                         }
                         if let amityError = AmityError(error: error), amityError == .noUserAccessPermission {
                             switch strongSelf.feedType {
